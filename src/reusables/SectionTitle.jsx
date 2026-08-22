1743954d535347e996d71a29b3a65cce6fd03f68
amrkhaled104 <amrkhaledelsisy@gmail.com>
2026-08-22 04:33:11 +0300

    feat(components): add reusable FormCard and SectionTitle components

---
+++ src/reusables/SectionTitle.jsx
@@ -0,0 +1,15 @@
+import "./SectionTitle.css";
+
+export default function SectionTitle({ title, subtitle = "", }) {
+    return (
+        <div className="section-title">
+            <h3>{title}</h3>
+
+            {subtitle && (
+                <p className="section-subtitle">
+                    {subtitle}
+                </p>
+            )}
+        </div>
+    );
+}
