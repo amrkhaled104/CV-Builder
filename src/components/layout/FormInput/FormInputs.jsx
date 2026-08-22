3a06c315aeddc02dc451b8eccb8c1e0b4d064a2d
amrkhaled104 <amrkhaledelsisy@gmail.com>
2026-08-22 04:33:24 +0300

    feat(layout): create FormInputs layout container

---
+++ src/components/layout/FormInput/FormInputs.jsx
@@ -0,0 +1,8 @@
+import "./FormInputs.css";
+import GeneralInfo from "../../../Forms/GeneralInfo.jsx";
+
+export default function FormInputs() {
+  return <section className="leftSidebarForm">
+    <GeneralInfo/>
+  </section>;
+}
