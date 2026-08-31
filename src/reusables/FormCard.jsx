ebcd716fd8dfb6aea24d32f7ab979f3e045f1842
amrkhaled104 <amrkhaledelsisy@gmail.com>
2026-08-31 04:36:43 +0300

    refactor

--- src/reusables/FormCard.jsx
+++ src/reusables/FormCard.jsx
@@ -1,9 +1,9 @@
 import "./FormCard.css";
 export default function FormCard({ title, children, className = "" }) {
   return (
-    <div className={`formCard ${className}`}>
+    <section className={`form-card ${className}`}>
       {title && <h2 className="form-card-title">{title}</h2>}
       <div className="form-card-content">{children}</div>
-    </div>
+    </section>
   );
 }
